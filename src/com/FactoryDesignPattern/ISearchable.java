3bff139bd4a3eeac138f6571f3b6dbf4212a43bd
Rohit Yadav <rohit.ece.nitj@gmail.com>
2016-12-26 19:08:50 +0530

    Deleting this Folder

--- src/com/FactoryDesignPattern/ISearchable.java
+++
@@ -1,7 +0,0 @@
-package com.FactoryDesignPattern;
-
-public interface ISearchable {
-	
-	// this may be removed 
-	ISearchable clone()  throws CloneNotSupportedException;
-}
--- src/com/FactoryDesignPattern/SearchType.java
+++
@@ -1,5 +0,0 @@
-package com.FactoryDesignPattern;
-
-public enum SearchType {
-
-}
